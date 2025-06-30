1d91e0b98890a6eba14c1c7920a541294d208256
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-06-30 21:56:37 +0300

    Time: 0 ms (100.00%) | Memory: 13.2 MB (30.68%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,29 +1,40 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def find_first():
-            left, right = 0, len(nums) - 1
-            res = -1
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """ 
+        def first(nums, target):
+            left = 0
+            right = len(nums) - 1
+            first_occurrence = -1
+
             while left <= right:
                 mid = (left + right) // 2
-                if nums[mid] >= target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
                 if nums[mid] == target:
-                    res = mid
-            return res
-        
-        def find_last():
-            left, right = 0, len(nums) - 1
-            res = -1
+                    first_occurrence = mid
+                    right = mid - 1  
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:  
+                    right = mid - 1
+            return first_occurrence
+
+        def last(nums, target):
+            left = 0
+            right = len(nums) - 1
+            last_occurrence = -1
+
             while left <= right:
                 mid = (left + right) // 2
-                if nums[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid - 1
                 if nums[mid] == target:
-                    res = mid
-            return res
-        
-        return [find_first(), find_last()]
+                    last_occurrence = mid
+                    left = mid + 1 
+                elif nums[mid] < target:
+                    left = mid + 1
+                else: 
+                    right = mid - 1
+            return last_occurrence
+
+        return [first(nums, target), last(nums, target)]
