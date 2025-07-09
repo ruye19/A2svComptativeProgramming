d0d3bf3e60f55476790869a2f2ad75a5cf1fb18d
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-07-09 13:31:17 +0300

    Time: 234 ms (83.29%) | Memory: 20.1 MB (68.36%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,28 +1,21 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        n,m=len(grid),len(grid[0])
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid)
+        cols=len(grid[0])
+        direction=[(1,0),(0,1),(-1,0),(0,-1)]
+        def dfs(i,j):
+            if i>=row or i<0 or j >=cols or j<0 or grid[i][j] !="1":
+                return
+            else:
+                grid[i][j]="0"
+                dfs(i,j+1)
+                dfs(i+1,j)
+                dfs(i,j-1)
+                dfs(i-1,j)
         island=0
-        def bfs(i,j):
-            q=deque()
-            q.append((i,j))
-            grid[i][j]="0"
-            while q:
-                x,y = q.popleft()
-                for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
-                    new_x,new_y= x+dx ,y+dy
-                    if 0 <= new_x < n and 0 <= new_y < m and grid[new_x][new_y] == "1":
-                        grid[new_x][new_y]="0" 
-                        q.append((new_x,new_y))
-                    
-        for i in range(n):
-           for j in range(m):
-             if grid[i][j]== "1":
-                island+=1
-                bfs(i,j)
-        return island         
-        
-        
+        for i in range(row):
+            for j in range(cols):
+                if grid[i][j]=="1":
+                    island+=1
+                    dfs(i,j)            
+        return island            
