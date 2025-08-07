f5ab11ee136c3984ca97c2ddd7dcd1b25427c6bd
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-08-07 21:00:17 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.51%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,13 +1,13 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        output=[]
+
         for i in range(len(nums)-1):
             if nums[i]==nums[i+1]:
-                output.append(nums[i]*2)
+                nums[i]=(nums[i]*2)
                 nums[i+1]=0
             else:    
-               output.append(nums[i]) 
-        output.append(nums[len(nums)-1])       
+               nums[i]=nums[i]
+          
         def zero_check(nums):
             non_zero=0
             for i in range(len(nums)):
@@ -15,5 +15,5 @@ class Solution:
                     nums[i],nums[non_zero]=nums[non_zero],nums[i]
                     non_zero+=1
             return nums   
-        return zero_check(output)      
+        return zero_check(nums)      
         
