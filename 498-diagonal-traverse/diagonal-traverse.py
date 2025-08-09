d430a56c9f87601975da3b5c916d558247e66512
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-08-09 22:57:24 +0300

    Time: 16 ms (34.39%) | Memory: 20.9 MB (20.81%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        n,m=len(mat),len(mat[0])
+        output=[]
+        sum_collection=defaultdict(list)
+        for i in range(n):
+            for j in range(m):
+                sum_collection[i+j].append(mat[i][j])
+                # if i+j not in sum_collection:
+                #     sum_collection[i+j]=[mat[i][j]]
+                # else: 
+                #     sum_collection[i+j].append(mat[i][j])   
+        for key,value in sum_collection.items():
+            if key%2==0:
+                output.extend(value[::-1])
+            else:
+                output.extend(value)  
+        return output
+
