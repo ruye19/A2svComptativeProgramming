96df5aa6cbe02f18d08343b1be4ecdabebec45fe
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-08-20 23:50:30 +0300

    Time: 231 ms (10.29%) | Memory: 19.4 MB (9.45%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,10 +1,17 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        for i in range(1, len(names)):
-            for j in range(i, 0, -1):
-                if heights[j] > heights[j - 1]:
-                    heights[j - 1], heights[j] = heights[j], heights[j - 1]
-                    names[j - 1], names[j] = names[j], names[j - 1]
-        return names            
+                    
+        hashnames = defaultdict(list)
+        for name, height in zip(names, heights):
+            hashnames[height].append(name)
             
-        
+        countArray = [0 for _ in range(max(heights) + 1)]
+        for height in heights:
+            countArray[height] += 1
+            
+        result = []
+        for i in range(len(countArray) - 1, -1, -1):
+            if countArray[i]:
+                result.extend(hashnames[i])
+            
+        return result
