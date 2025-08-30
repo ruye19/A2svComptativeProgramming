ed1309688b034888dc5afdce5ad9db0f50f70be9
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-08-30 17:35:19 +0300

    Time: 95 ms (31.58%) | Memory: 27.2 MB (95.68%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def findMaxAverage(self, s, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: float
-        """
-
-        window=sum(s[:k])
-        maxx=window
-        for r in range(k,len(s)):
-            window= window- s[r-k] + s[r] 
-            maxx=max(window,maxx)
-        return float(maxx)/k   
-        
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window = 0
+        for i in range(k):
+            window += nums[i]
+        maxumim = window      
+        for j in range(k, len(nums)):
+            window = window - nums[j - k] + nums[j] 
+            maxumim = max(window , maxumim)
+        return  maxumim / k       
