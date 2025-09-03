61e5fd68a1ae6ad358448408886925f833fbbc7d
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-09-03 19:49:55 +0300

    Time: 31 ms (62.19%) | Memory: 20 MB (99.76%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,23 +1,13 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        n = len(nums)
-        prefixsum = 0
-        count = 0
-        prefixsumm = {0: 1} 
-
-        for i in range(n): 
-            prefixsum += nums[i]  
-           
-            if (prefixsum - k) in prefixsumm:
-                count += prefixsumm[prefixsum - k]  
-            if prefixsum in prefixsumm:
-                prefixsumm[prefixsum] += 1
-            else:
-                prefixsumm[prefixsum] = 1
-
-        return count  
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix = nums
+        for i in range(1 , len(nums)):
+            prefix[i] += prefix[i - 1]
+        hashmap = {0 : 1}
+        result = 0 
+        for p in prefix:
+            target = p - k
+            result += hashmap.get(target, 0)
+            hashmap[p] = hashmap.get(p, 0) + 1
+        return result         
+        
