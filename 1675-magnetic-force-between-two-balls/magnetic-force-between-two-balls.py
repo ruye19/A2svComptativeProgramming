8f355544ad825bde444de100ddd2d018f05d37d5
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-09-14 05:17:13 +0300

    Time: 523 ms (28.38%) | Memory: 30.4 MB (83.54%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        def can_distribute(mi,balls):
+            count = 1
+            prev = 0
+            for i in range(len(position)):
+                if position[i] - position[prev] >= mi:
+                    count += 1
+                    prev = i
+            return count >= balls 
+        position = sorted(position)     
+        left, right = 0, max(position)
+        while left <= right:
+            mid = (right + left) // 2
+            if can_distribute(mid, m):
+                left = mid + 1
+            else:
+                right = mid - 1   
+        return right        
+
+
+
+        
