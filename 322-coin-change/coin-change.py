3476d8c594afd2aa99152dc0cd8ad287694cc1b4
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-09-27 01:25:51 +0300

    Time: 1665 ms (5.01%) | Memory: 42.2 MB (14.03%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,12 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def dp(i,amount):
-            if amount == 0:
+        memo = [[-1 for _ in range(len(coins))] for _ in range(amount)]
+        # SC: O(i * amount) = 12 * 10**4 = 120_000bytes
+      
+        def dp(i, total):
+            if total == amount:
                 return 0
-            if (i == len(coins) and amount > 0) or amount < 0 :
+            if (i == len(coins) and total < amount) or total > amount :
                 return float('inf') 
-           
-            return min(1 + dp(i, amount - coins[i]), dp(i+1, amount))  
-        h = dp(0,amount)    
+               
+            if memo[total][i] == -1:
+                memo[total][i] = min(1 + dp(i, total + coins[i]), dp(i+1, total))  
+            return memo[total][i]
+        h = dp(0, 0)    
         return h if h != float('inf') else -1 
