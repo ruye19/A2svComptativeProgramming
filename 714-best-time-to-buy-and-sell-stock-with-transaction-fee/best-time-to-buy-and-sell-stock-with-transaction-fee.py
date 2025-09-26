6e1d9097c1317d480faa0e3fa6711404247fc3dd
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-09-26 21:11:44 +0300

    Time: 727 ms (5.16%) | Memory: 201.9 MB (11.82%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        @cache
+        memo = {}
         def dp(i, h):
             if i == len(prices):
                 return 0 
-                    
-            if not h:
-                return max(-prices[i] + dp(i + 1, True),dp(i + 1, h))
-            else:
-                return max(prices[i] - fee + dp(i + 1, False), dp(i + 1, h)) 
-
+            if (i,h) not in memo:
+                if not h:
+                    memo[(i,h)] = max(-prices[i] + dp(i + 1, True),dp(i + 1, h))
+                else:
+                    memo[(i,h)] = max(prices[i] - fee + dp(i + 1, False), dp(i + 1, h)) 
+            return memo[(i,h)]
         return dp(0, False)
