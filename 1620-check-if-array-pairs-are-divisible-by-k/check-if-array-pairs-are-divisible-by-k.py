c729dc8c5490741e9b11b8a4f91f2428c14c2ad9
Ruth Yeshitila <167624441+ruye19@users.noreply.github.com>
2025-10-12 20:20:20 +0300

    Time: 43 ms (80.71%) | Memory: 31.7 MB (89.80%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -1,13 +1,15 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        freq = [0 for i in range(k)]
+                  
+        rem = [0 for _ in range(k)]
         for num in arr:
-            freq[num % k] += 1
-        for num in range(1, k):
-            if num == k - num and freq[num] & 1:
-                return False
-            if freq[num] != freq[k - num]:
-                return False
-        return True            
-
+            rem[num%k] += 1
+            
+        # k = 6
+        # 0 1 2 3 4 5
         
+        for i in range(1, k):
+            if rem[i] != rem[k - i] or (k%2 == 0 and rem[k//2] % 2 != 0):
+                return False
+                
+        return True
